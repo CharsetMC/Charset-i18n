0a62300795d1100184cf614135cc55e7e192ee1d
Adrian Siekierka <kontakt@asie.pl>
2017-07-02 11:50:08 +0200

    fix compare.sh for 1.12

--- compare.sh
+++ compare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sed 's/=.*//g' ../src/main/resources/assets/charsetlib/lang/en_US.lang > /tmp/charset_keys1.lang
-sed 's/=.*//g' assets/charsetlib/lang/$1.lang > /tmp/charset_keys2.lang
+sed 's/=.*//g' ../src/main/resources/assets/charset/lang/en_us.lang > /tmp/charset_keys1.lang
+sed 's/=.*//g' assets/charset/lang/$1.lang > /tmp/charset_keys2.lang
 diff -u /tmp/charset_keys1.lang /tmp/charset_keys2.lang
 rm /tmp/charset_keys1.lang
 rm /tmp/charset_keys2.lang
