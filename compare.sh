b231855848d1e2fcfc820cb6320d9fa12933dbda
asie <kontakt@asie.pl>
2018-04-25 14:41:45 +0200

    add license to compare script

--- compare.sh
+++ compare.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
+#
+# Copyright (c) 2015, 2016, 2017, 2018 Adrian Siekierka
+#
+# This file is part of Charset.
+#
+# Charset is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Charset is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Charset.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 sed 's/=.*//g' ../src/main/resources/assets/charset/lang/en_us.lang > /tmp/charset_keys1.lang
 sed 's/=.*//g' assets/charset/lang/$1.lang > /tmp/charset_keys2.lang
 diff -u /tmp/charset_keys1.lang /tmp/charset_keys2.lang
